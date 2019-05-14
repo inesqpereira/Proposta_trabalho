700339276044be137a8e9f6c7c52317280ac6139
inesqpereira <49939561+inesqpereira@users.noreply.github.com>
2019-05-14 20:27:20 +0100

    Add files via upload

---
+++ Login.vb
@@ -0,0 +1,8 @@
+﻿Public Class Login
+    Private Sub Button_entrar_Click(sender As Object, e As EventArgs) Handles Button_entrar.Click
+
+        Dim Form As New FormPrincipal
+
+        Form.Show()
+    End Sub
+End Class
